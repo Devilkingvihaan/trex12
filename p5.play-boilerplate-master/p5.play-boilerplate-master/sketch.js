40e3997e19d0ab0632a091c6b080c8435c42616d
Devilkingvihaan <63194671+Devilkingvihaan@users.noreply.github.com>
2020-04-25 13:43:18 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,28 @@
+var mrect,srect;
+function setup() {
+  createCanvas(800,400);
+ mrect =  createSprite(400, 200, 50, 50);
+ mrect.shapeColor="red";
+ srect = createSprite(200,200,30,40);
+ srect.shapeColor="blue";
+}
+
+function draw() {
+  background(255,255,255);  
+  mrect.x=mouseX;
+  mrect.y=mouseY;
+
+  if(mrect.x-srect.x<mrect.width/2 +srect.width/2 && 
+    srect.x-mrect.x<mrect.width/2 +srect.width/2 &&
+    mrect.y-srect.y<mrect.height/2 + srect.height/2 &&
+    srect.y-mrect.y<mrect.height/2 +srect.height/2
+    ){
+      mrect.shapeColor="green";
+      srect.shapeColor="blue";
+    }
+    else{
+      mrect.shapeColor="red";
+      srect.shapeColor="black";
+    }
+  drawSprites();
+}
